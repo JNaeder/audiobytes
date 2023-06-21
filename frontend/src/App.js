2b29365bf4df79a611342f12384e8b4db26654df
jnaederSAE <j.naeder@sae.edu>
2023-06-21 14:23:38 -0400

    Moved files to new front end folder

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Typography variant="h3">audiobytes.app</Typography>
+    </>
+  );
+}
+
+export default App;
